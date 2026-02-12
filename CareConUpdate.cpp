f8d7228e0a794b3295de9b7c1560de444dc0d1fe
So Ryoung Kim <hera3008@naver.com>
2026-02-12 15:40:34 +0900

    관리자권한 부여

--- CareConUpdate.cpp
+++ CareConUpdate.cpp
@@ -40,6 +40,25 @@ CCareConUpdateApp theApp;
 
 BOOL CCareConUpdateApp::InitInstance()
 {
+	HANDLE hToken = nullptr;
+	TOKEN_ELEVATION elevation = {};
+	DWORD dwSize = 0;
+	if (OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &hToken))
+	{
+		if (GetTokenInformation(hToken, TokenElevation, &elevation, sizeof(elevation), &dwSize) &&
+			elevation.TokenIsElevated == 0)
+		{
+			TCHAR strPath[MAX_PATH] = { 0, };
+			::GetModuleFileName(nullptr, strPath, MAX_PATH);
+			if ((INT_PTR)ShellExecute(nullptr, _T("runas"), strPath, nullptr, nullptr, SW_SHOWNORMAL) > 32)
+			{
+				CloseHandle(hToken);
+				return FALSE;
+			}
+		}
+		CloseHandle(hToken);
+	}
+
 	// 애플리케이션 매니페스트가 ComCtl32.dll 버전 6 이상을 사용하여 비주얼 스타일을
 	// 사용하도록 지정하는 경우, Windows XP 상에서 반드시 InitCommonControlsEx()가 필요합니다.
 	// InitCommonControlsEx()를 사용하지 않으면 창을 만들 수 없습니다.
